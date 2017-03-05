8951f9cc96f0fe688d2a1e8204007ee26a07617c
marcsburt <marc.s.burt@marcsburt.com>
2017-03-05 16:38:27 -0500

    added shares to blog list

--- src/app/myBlog/frontend/common/post-list/post-preview.controller.js
+++ src/app/myBlog/frontend/common/post-list/post-preview.controller.js
@@ -1,9 +1,15 @@
-function BlogPostController() {
+function BlogPostController($sce) {
 	var ctrl = this;
 	ctrl.$onInit = function (){
 		ctrl.posts = ctrl.postData
+		ctrl.popoverShare = {
+			templateUrl: './share-popover.html'
+		};
 	}
 
+	ctrl.shared = function(){
+		console.log('got clicked, eh')
+	}
 }
 
 angular
